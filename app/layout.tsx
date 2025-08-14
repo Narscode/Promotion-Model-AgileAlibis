abfd5847ad1847e5995d5975bea4683d82e8d3a7
Nareswari Ayu Prabowo <144470962+Narscode@users.noreply.github.com>
2025-08-14 20:08:35 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,9 @@ import { GeistMono } from 'geist/font/mono'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.app',
+  title: 'Agile Alibis Model',
+  description: 'Operational and Promotion',
+  generator: '',
 }
 
 export default function RootLayout({
